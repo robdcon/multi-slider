e981da5c326d30e2af231b3576b429d58c14b68f
robdcon <robdcon@gmail.com>
2019-10-02 11:40:26 +0100

    increment / decrement functions working

---
+++ multislider.js
@@ -0,0 +1,94 @@
+$(document).ready(function()
+{
+	
+	var slideControls = slider()
+	$('#right').on('click', slideControls.increment)
+	$('#left').on('click', slideControls.decrement)
+	//test()
+	
+})
+
+// function test()
+// {
+// 	console.log('test-working')
+// }
+
+function slider()
+{
+	
+	
+	let slideLength = 200
+	let position = 0
+	
+	//console.log(slideLength)
+	const $slideTrack = $('.slide-container-inner')
+	$('.slide-container-inner').children().first().addClass('active')
+	let next = $('.active').next()
+	let prev = $('.active').prev()
+	const slides = $('.slide-container-inner').children()
+	const length = $('.slide-container-inner').children().length
+	
+					
+	return  {
+
+		
+		decrement: ()=>
+		{
+			if($('.active').index() - 1 < 0)
+			{
+				position = slideLength * (length - 1 )
+				
+				console.log("POS: ", position)
+				//console.log( $('.active').index() )
+
+				$('.active').removeClass('active')
+				slides.last().addClass('active')
+				$slideTrack.css('transform', 'translateX('+ position +')')
+			}
+			else
+			{
+				
+				position = position - slideLength
+				console.log("POS: ", position)
+				
+				//console.log( $('.active').index() )
+
+				$('.active').removeClass('active').prev().addClass('active')
+				$slideTrack.css('transform', 'translateX('+ position +')')
+			}
+		},	
+			
+		
+
+		increment: ()=>
+		{
+			if($('.active').index() + 1 > length -1)
+			{
+				position = 0
+				
+				console.log("POS: ", position)
+				//console.log( $('.active').index() )
+
+				$('.active').removeClass('active')
+				slides.first().addClass('active')
+				$slideTrack.css('transform', 'translateX('+ position +')')
+				
+			}
+			else
+			{
+				position = position + slideLength
+				
+				 console.log("POS: ", position)
+				//console.log( $('.active').index() )
+
+				$('.active').removeClass('active').next().addClass('active')
+				$slideTrack.css('transform', 'translateX('+ position +')')
+			}
+		},
+		getPos: () => {return position},
+
+	}			
+				
+}
+
+
